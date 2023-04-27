f83869f8d9eca27b0187312897da79032f5ec760
Vitor Fraga <vitor.augusto.fraga@gmail.com>
2023-04-27 19:00:43 -0300

    First commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Open Data Platform'
+copyright = '2023, Vitor Fraga'
+author = 'Vitor Fraga'
 
 release = '0.1'
 version = '0.1.0'
